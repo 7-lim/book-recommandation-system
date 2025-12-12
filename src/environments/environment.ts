49354eac41d731f078cad5d2fdf4b277b1d2e3ee
7-lim <130911391+7-lim@users.noreply.github.com>
2025-12-12 20:40:22 +0100

    Obscure Firebase credentials in environment.ts
    Replaced Firebase credentials with placeholders for security.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyCGfX-49-DgJdE6Z8YTVjk90ktb2fx2iNs",
-        authDomain: "book-recommandation-syst-ee1ca.firebaseapp.com",
-        projectId: "book-recommandation-syst-ee1ca",
-        storageBucket: "book-recommandation-syst-ee1ca.firebasestorage.app",
-        messagingSenderId: "940780452394",
-        appId: "1:940780452394:web:6bc866d8215c0066cfa153",
-        measurementId: "G-V48R5C91N5"
+        apiKey: "############################",
+        authDomain: "############################",
+        projectId: "############################",
+        storageBucket: "############################",
+        messagingSenderId: "############################",
+        appId: "############################",
+        measurementId: "############################"
     }
 };
